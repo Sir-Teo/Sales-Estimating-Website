69f6d332b71942ea5cc4d335c2b7a083393e0f35
Teo <32597645+Sir-Teo@users.noreply.github.com>
2024-07-15 11:24:05 -0400

    u0pdated description

--- frontend/src/components/PredictionForm.js
+++ frontend/src/components/PredictionForm.js
@@ -5,103 +5,218 @@ import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const groupsManual = {
-  'Master_Controllers': ['CO01', 'CO06', 'AN09', 'SX01', 'TD01', 'DC01', 'DC02', 'DC09'],
-  'Field_Controllers': ['SX05', 'SX06'],
-  'VAV_Controllers': ['AN04', 'AN05', 'AN06', 'DC03', 'DC04', 'DC05', 'SX04'],
-  'Sensors': ['DA01', 'DA02', 'DA03', 'DM01', 'DT01', 'DT02', 'DT03', 'DT04', 'DT05', 'DT06', 'DT07', 'DT08', 'DT09', 'HS01', 'HS02', 'HS03', 'HS04', 'MD01', 'MD02', 'SE01', 'SE02', 'SE03', 'SE04', 'SE05', 'SE06', 'SE07', 'SE08', 'SE09', 'SE10', 'SE11', 'SE12', 'SS01', 'SS02', 'ST01', 'ST02', 'ST03', 'ST04', 'DC06', 'DC07', 'SW01', 'SW02', 'SW03', 'SW04', 'SW05', 'SW06', 'TC01', 'TC02', 'TC03', 'TC04', 'TC05', 'TR01', 'TR02', 'TR03', 'TS01', 'TS02', 'TS03', 'TS04', 'TS05', 'VL01', 'VL02'],
-  'Panels': ['EN01'],
-  'Software': ['CO07', 'CP02', 'DC08', 'CP06', 'SX02', 'TD02'],
-  'Computers': ['CP01', 'CP05'],
+  'AN ANDOVER EQUIPMENT': ['AN01',
+    'AN02',
+    'AN03',
+    'AN04',
+    'AN05',
+    'AN06',
+    'AN07',
+    'AN08',
+    'AN09'],
+  'BA1 BUILDING ANALYTICS': ['BA01'],
+  'BD1 BONDING': ['BD01'],
+  'CO CONTINUUM': ['CO01',
+    'CO02',
+    'CO03',
+    'CO04',
+    'CO05',
+    'CO06',
+    'CO07',
+    'CO08',
+    'CO09'],
+  'CP COMPUTER': ['CP01', 'CP02', 'CP03', 'CP04', 'CP05', 'CP06', 'CP07'],
+  'DA DAMPER ACTUATORS': ['DA01', 'DA02', 'DA03'],
+  'DC DISTECH CONTROLS': ['DC01',
+    'DC02',
+    'DC03',
+    'DC04',
+    'DC05',
+    'DC06',
+    'DC07',
+    'DC08',
+    'DC09'],
+  'DM1 DAMPERS': ['DM01'],
+  'DT DETECTOR': ['DT01',
+    'DT02',
+    'DT03',
+    'DT04',
+    'DT05',
+    'DT06',
+    'DT07',
+    'DT08',
+    'DT09'],
+  'EN1 ENCLOSURES': ['EN01'],
+  'FB1 PANEL FAB MATERIALS': ['FB01'],
+  'HS HUMIDITY SENSORS (RH)': ['HS01', 'HS02', 'HS03', 'HS04'],
+  'IM INSTALLATION MATERIAL': ['IM01', 'IM02'],
+  'MD MOTORS AND DRIVES': ['MD01', 'MD02'],
+  'MG1 METERS/GAUGES': ['MG01'],
+  'OM OTHER MATERIALS': ['OM01', 'OM02', 'OM03'],
+  'SB SUB-CONTRACTORS': ['SB01', 'SB02', 'SB03', 'SB04'],
+  'SE SECURITY': ['SE01',
+    'SE10',
+    'SE11',
+    'SE12',
+    'SE02',
+    'SE03',
+    'SE04',
+    'SE05',
+    'SE06',
+    'SE07',
+    'SE08',
+    'SE09'],
+  'SS SWITCHES/SIGNAL': ['SS01', 'SS02'],
+  'ST STATS': ['ST01', 'ST02', 'ST03', 'ST04'],
+  'SW SWITCHES/CONTROLLER': ['SW01', 'SW02', 'SW03', 'SW04', 'SW05', 'SW06'],
+  'SX STRUXUREWARE': ['SX01', 'SX02', 'SX03', 'SX04', 'SX05', 'SX06'],
+  'TC TRANSDUCERS/CONTROLLER': ['TC01', 'TC02', 'TC03', 'TC04', 'TC05'],
+  'TD TRIDIUM/JACE': ['TD01', 'TD02', 'TD03'],
+  'TL1 TELECOM/SERVICE PROVIDER': ['TL01'],
+  'TO1 TOOLS': ['TO01'],
+  'TR TRANSDUCER/SIGNAL': ['TR01', 'TR02', 'TR03'],
+  'TS TEMPERATURE SENSORS': ['TS01', 'TS02', 'TS03', 'TS04', 'TS05'],
+  'TX1 SALES TAX': ['TX01'],
+  'UA1 UNALLOCATED COSTS': ['UA01'],
+  'VL VALVES': ['VL01', 'VL02'],
+  'WI1 WIRE': ['WI01'],
+  'WR1 WARRANTY': ['WR01']
 };
 
-const descriptions = {
-  'CO01': 'Description for CO01',
-  'CO06': 'Description for CO06',
-  'AN09': 'Description for AN09',
-  'SX01': 'Description for SX01',
-  'TD01': 'Description for TD01',
-  'DC01': 'Description for DC01',
-  'DC02': 'Description for DC02',
-  'DC09': 'Description for DC09',
-  'SX05': 'Description for SX05',
-  'SX06': 'Description for SX06',
-  'AN04': 'Description for AN04',
-  'AN05': 'Description for AN05',
-  'AN06': 'Description for AN06',
-  'DC03': 'Description for DC03',
-  'DC04': 'Description for DC04',
-  'DC05': 'Description for DC05',
-  'SX04': 'Description for SX04',
-  'DA01': 'Description for DA01',
-  'DA02': 'Description for DA02',
-  'DA03': 'Description for DA03',
-  'DM01': 'Description for DM01',
-  'DT01': 'Description for DT01',
-  'DT02': 'Description for DT02',
-  'DT03': 'Description for DT03',
-  'DT04': 'Description for DT04',
-  'DT05': 'Description for DT05',
-  'DT06': 'Description for DT06',
-  'DT07': 'Description for DT07',
-  'DT08': 'Description for DT08',
-  'DT09': 'Description for DT09',
-  'HS01': 'Description for HS01',
-  'HS02': 'Description for HS02',
-  'HS03': 'Description for HS03',
-  'HS04': 'Description for HS04',
-  'MD01': 'Description for MD01',
-  'MD02': 'Description for MD02',
-  'SE01': 'Description for SE01',
-  'SE02': 'Description for SE02',
-  'SE03': 'Description for SE03',
-  'SE04': 'Description for SE04',
-  'SE05': 'Description for SE05',
-  'SE06': 'Description for SE06',
-  'SE07': 'Description for SE07',
-  'SE08': 'Description for SE08',
-  'SE09': 'Description for SE09',
-  'SE10': 'Description for SE10',
-  'SE11': 'Description for SE11',
-  'SE12': 'Description for SE12',
-  'SS01': 'Description for SS01',
-  'SS02': 'Description for SS02',
-  'ST01': 'Description for ST01',
-  'ST02': 'Description for ST02',
-  'ST03': 'Description for ST03',
-  'ST04': 'Description for ST04',
-  'DC06': 'Description for DC06',
-  'DC07': 'Description for DC07',
-  'SW01': 'Description for SW01',
-  'SW02': 'Description for SW02',
-  'SW03': 'Description for SW03',
-  'SW04': 'Description for SW04',
-  'SW05': 'Description for SW05',
-  'SW06': 'Description for SW06',
-  'TC01': 'Description for TC01',
-  'TC02': 'Description for TC02',
-  'TC03': 'Description for TC03',
-  'TC04': 'Description for TC04',
-  'TC05': 'Description for TC05',
-  'TR01': 'Description for TR01',
-  'TR02': 'Description for TR02',
-  'TR03': 'Description for TR03',
-  'TS01': 'Description for TS01',
-  'TS02': 'Description for TS02',
-  'TS03': 'Description for TS03',
-  'TS04': 'Description for TS04',
-  'TS05': 'Description for TS05',
-  'VL01': 'Description for VL01',
-  'VL02': 'Description for VL02',
-  'EN01': 'Description for EN01',
-  'CO07': 'Description for CO07',
-  'CP02': 'Description for CP02',
-  'DC08': 'Description for DC08',
-  'CP06': 'Description for CP06',
-  'SX02': 'Description for SX02',
-  'TD02': 'Description for TD02',
-  'CP01': 'Description for CP01',
-  'CP05': 'Description for CP05',
-};
+const descriptions = {'AN01': 'Repair',
+  'AN02': 'Advance replacement',
+  'AN03': 'Infilink controller',
+  'AN04': '12 controller',
+  'AN05': 'B3 controller',
+  'AN06': 'Expansion modules',
+  'AN07': 'Roamio/roamio-2',
+  'AN08': 'Blink repeater',
+  'AN09': 'Agx controller',
+  'BA01': 'Building analytics',
+  'BD01': 'Bonding',
+  'CO01': 'Net controller',
+  'CO02': 'Power supply',
+  'CO03': 'I/o modules',
+  'CO04': 'Enclosures',
+  'CO05': 'Displays',
+  'CO06': 'Bcx master',
+  'CO07': 'Software',
+  'CO08': 'Xdriver',
+  'CO09': 'Software upgrade',
+  'CP01': 'Workstation',
+  'CP02': 'Software',
+  'CP03': 'Printer',
+  'CP04': 'Battery back-up/ups',
+  'CP05': 'Server',
+  'CP06': 'Software upgrade',
+  'CP07': 'Network switch',
+  'DA01': 'Electric',
+  'DA02': 'Pneumatic',
+  'DA03': 'Endswitch',
+  'DM01': 'Dampers',
+  'DT01': 'Firestat',
+  'DT02': 'Leak (water)',
+  'DT03': 'Duct smoke',
+  'DT04': 'Space smoke',
+  'DT05': 'Tank level',
+  'DT06': 'Co2 - carbon dioxide',
+  'DT07': 'Alarm module',
+  'DT08': 'Carbon monoxide',
+  'DT09': 'Methane sensor',
+  'EN01': 'Enclosures',
+  'FB01': 'Panel fab materials',
+  'HO01': 'No Description',
+  'HO02': 'Design',
+  'HO03': 'Fabrication',
+  'HO04': 'Programming - control',
+  'HO05': 'Project mgmt',
+  'HO06': 'Training',
+  'HO07': 'Programming - frontend',
+  'HO08': 'Pneumatic',
+  'HO09': 'Controls technician',
+  'HO10': 'Electrical technician',
+  'HO11': 'Other labor',
+  'HO12': 'Start up',
+  'HS01': 'Space',
+  'HS02': 'Duct',
+  'HS03': 'Immersion',
+  'HS04': 'Outdoor',
+  'IM01': 'Electric',
+  'IM02': 'Pneumatic',
+  'MD01': 'Motors',
+  'MD02': 'Drives',
+  'MG01': 'Meters/gauges',
+  'SB01': 'Electricians',
+  'SB02': 'Pneumatic',
+  'SB03': 'Steamfitters',
+  'SB04': 'Miscellaneous',
+  'SE01': 'Camera',
+  'SE02': 'Monitor',
+  'SE03': 'Dvr',
+  'SE04': 'Maglock',
+  'SE05': 'Door strike',
+  'SE06': 'Software',
+  'SE07': 'Software upgrade',
+  'SE08': 'Card readers',
+  'SE09': 'Id cards',
+  'SE10': 'Miscellaneous',
+  'SE11': 'Aiphone equipment',
+  'SE12': 'Door contacts, pirs',
+  'SS01': 'Relay',
+  'SS02': 'High limit',
+  'ST01': 'Freezestat',
+  'ST02': 'Aquastat',
+  'ST03': 'Thermostat',
+  'ST04': 'Firestat',
+  'DC01': 'Apex',
+  'DC02': 'S1000 -master controller',
+  'DC03': 'I/o module',
+  'DC04': 'Controller',
+  'DC05': 'Ecy - vav controller',
+  'DC06': 'Space sensors',
+  'DC07': 'Thermostat',
+  'DC08': 'Software',
+  'DC09': 'Jace - distech',
+  'SW01': 'Duct static',
+  'SW02': 'Current',
+  'SW03': 'Water pressure',
+  'SW04': 'Fan status (air dp)',
+  'SW05': 'Filter status - air flow',
+  'SW06': 'Water flow',
+  'SX01': 'As',
+  'SX02': 'Server software (es, rs)',
+  'SX03': 'Power supply',
+  'SX04': 'I/o module',
+  'SX05': 'Mpc controller',
+  'SX06': 'Mpv vav controller',
+  'TC01': 'Duct static',
+  'TC02': 'Water pressure',
+  'TC03': 'Steam pressure',
+  'TC04': 'Current',
+  'TC05': 'Air flow - (afmd)',
+  'TD01': 'Jace controllers',
+  'TD02': 'Enterprise software',
+  'TD03': 'Tridium miscellaneous',
+  'TL01': 'Telecom/service provider',
+  'TO01': 'Tools',
+  'TR01': 'Current to pneumatic (i/p)',
+  'TR02': 'Elec. to pneumatic (i/p)',
+  'TR03': 'Pneumatic to elec. (p/e)',
+  'TS01': 'Space',
+  'TS02': 'Duct',
+  'TS03': 'Immersion - water',
+  'TS04': 'Outdoor',
+  'TS05': 'Carbon dioxide',
+  'TX01': 'Sales tax',
+  'VL01': 'Electrical',
+  'VL02': 'Pneumatic',
+  'WI01': 'Wire',
+  'WR01': 'Warranty',
+  'OM01': 'Other materials',
+  'OM02': 'Repair',
+  'OM03': 'Advance replacement',
+  'UA01': 'Unallocated costs'};
+
 
 const PredictionForm = ({ onSubmit }) => {
   const [formData, setFormData] = useState([]);
@@ -143,7 +258,7 @@ const PredictionForm = ({ onSubmit }) => {
         <Grid item xs={12} sm={8}>
           <FormControl fullWidth>
             <Autocomplete
-              options={Object.entries(groupsManual).flatMap(([group, codes]) => 
+              options={Object.entries(groupsManual).flatMap(([group, codes]) =>
                 codes.map(code => ({ group, code, description: descriptions[code] }))
               )}
               groupBy={(option) => option.group}
@@ -156,11 +271,11 @@ const PredictionForm = ({ onSubmit }) => {
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Button 
+          <Button
             fullWidth
-            variant="contained" 
-            color="primary" 
-            onClick={handleAddItem} 
+            variant="contained"
+            color="primary"
+            onClick={handleAddItem}
             startIcon={<AddIcon />}
             disabled={!selectedItem}
           >
@@ -168,7 +283,7 @@ const PredictionForm = ({ onSubmit }) => {
           </Button>
         </Grid>
       </Grid>
-      
+
       <Box sx={{ mt: 3 }}>
         {formData.map((item, index) => (
           <Tooltip key={index} title={descriptions[item.name]} arrow>
@@ -182,7 +297,7 @@ const PredictionForm = ({ onSubmit }) => {
           </Tooltip>
         ))}
       </Box>
-      
+
       {formData.length > 0 && (
         <Grid container spacing={2} sx={{ mt: 2 }}>
           {formData.map((item, index) => (
@@ -200,11 +315,11 @@ const PredictionForm = ({ onSubmit }) => {
           ))}
         </Grid>
       )}
-      
-      <Button 
-        type="submit" 
-        variant="contained" 
-        color="primary" 
+
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
         onClick={handleSubmit}
         disabled={formData.length === 0 || formData.some(item => !item.quantity)}
         sx={{ mt: 3 }}
