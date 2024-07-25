a4d50ba61c9be615fff5c67fe250d718d993a8e9
Teo <32597645+Sir-Teo@users.noreply.github.com>
2024-07-25 15:26:13 -0400

    fixed env vars

--- init-db.sh
+++ init-db.sh
@@ -2,7 +2,5 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER test_user WITH PASSWORD 'test_password';
-    CREATE DATABASE test_db;
     GRANT ALL PRIVILEGES ON DATABASE test_db TO test_user;
 EOSQL
